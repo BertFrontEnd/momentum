e2f7d28a2386853523fb911a15647f08972d4ea9
BertFrontEnd <Berton@yandex.ru>
2021-10-26 01:03:01 +0300

    chore: edit main script

--- momentum/script.js
+++ momentum/script.js
@@ -1,3 +1,7 @@
+alert(
+  'If possible, postpone the check until Thursday, maybe after 15.00, please. Or let me know the deadline when you can check (https://t.me/BertCAD). Thank you very much!',
+);
+
 import { renderForecast, setForecast } from './assets/modules/weather.js';
 import { setVisibility } from './assets/modules/visibility.js';
 import { renderPhrase, setPhrase } from './assets/modules/quote.js';
